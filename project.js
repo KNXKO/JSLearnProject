1db25f2d70b99baaec49f07ef1435b12a24098f8
KNXKO <lukaslechovic@gmail.com>
2024-02-10 12:28:05 +0100

    Added numberoflines function

--- project.js
+++ project.js
@@ -19,4 +19,19 @@ const deposit = () => {
     }
   }
 };
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
 const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
